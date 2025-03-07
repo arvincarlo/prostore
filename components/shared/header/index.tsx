07510cdd4798a7df9e1248b07d67b7882a819f3c
Arvin <acris@vertere-gs.com>
2025-03-07 16:55:49 +0800

    Responsive Sheet Menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, User } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
 import Image from "next/image";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
     return <header className="w-full border-b">
@@ -19,19 +17,7 @@ const Header = () => {
                     <span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
                 </Link>
             </div>
-            <div className="space-x-2">
-                <ModeToggle></ModeToggle>
-                <Button asChild variant="ghost">
-                    <Link href="/cart">
-                        <ShoppingCart/> Cart
-                    </Link>
-                </Button>
-                <Button asChild>
-                    <Link href="/sign-in">
-                        <User/> Sign In
-                    </Link>
-                </Button>
-            </div>
+            <Menu/>
         </div>
     </header>
 }
