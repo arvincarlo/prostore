14e71d3f5a54bca378e5bae046ecb20ed1a1ec19
Arvin <acris@vertere-gs.com>
2025-03-10 14:05:03 +0800

    Product Price Component

---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+const ProductPrice = ({ value, className }: { value: number; className?: string; }) => {
+    // Ensure 2 decimal places
+    const stringValue = value.toFixed(2);
+
+    // Get the int/float
+    const [intValue, floatValue] = stringValue.split('.');
+
+    return (
+        <p className={cn('text-2xl', className)}>
+            <span className="text-xs align-super">P</span>
+            {intValue}
+            <span className="text-xs align-super">.{floatValue}</span>
+        </p>
+    );
+}
+
+export default ProductPrice;
