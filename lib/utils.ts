ca1eceb151b54e97e331a7ef1ddb502a54ad93db
Arvin <acris@vertere-gs.com>
2025-03-10 17:18:55 +0800

    Zod Validation & Type Inference

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+// Format number with decimal places
+export function formatNumberWithDecimal(num: string) {
+  const [int, decimal] = num.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`
+}
