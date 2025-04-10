79e95a8ea2f0a53c0c5ae880ffa860e9acc21b12
Arvin <ACRCRIS@chinabank.ph>
2025-04-10 14:28:20 +0800

    Cart Zod Schema & Prisma Model

--- lib/validators.ts
+++ lib/validators.ts
@@ -36,4 +36,24 @@ export const signUpFormSchema = z
 .refine((data) => data.password === data.confirmPassword, {
     message: 'Passwords do not match',
     path: ['confirmPassword']
+});
+
+// Cart Schemas
+export const cartItemSchema = z.object({
+    productId: z.string().min(1, 'Product is required'),
+    name: z.string().min(1, 'Name is required'),
+    slug: z.string().min(1, 'Slug is required'),
+    qty: z.number().int().nonnegative('Quatity must be atleast 1'),
+    image: z.string().min(1, 'Image is required'),
+    price:  currency
+});
+
+export const insertCartSchema = z.object({
+    items: z.array(cartItemSchema),
+    itemsPrice: currency,
+    totalPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    sesionCartId: z.string().min(1, 'Session cart id is required'), 
+    userId: z.string().optional().nullable()
 })
